7863477f100a7e5a5a00ce74eb2c0c27c4705231
John Mark Tizado <johnmarktizado@gmail.com>
2024-04-24 01:49:49 +0800

    add: working read and write code to firestore

---
+++ gettingTesting/gettingTesting.ino
@@ -0,0 +1,217 @@
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WIFI.h>
+#endif
+
+#include <Firebase_ESP_Client.h>
+#include <addons/TokenHelper.h>
+#include <ArduinoJson.h>
+
+#include <Arduino.h>
+#include "soc/rtc.h"
+#include <HX711_ADC.h>
+
+  // WIFI Credentials
+  #define WIFI_SSID "virusX"
+  #define WIFI_PASSWORD "simacmacmagbabayad"
+
+  // DEFINE FIREBASE API KEY, PROJECT ID
+  #define API_KEY "AIzaSyDPcMRU9x421wP0cS1sRHwEvi57W8NoLiE"
+  #define FIREBASE_PROJECT_ID "petness-92c55"
+
+  // USER CREDENTIALS
+  #define USER_EMAIL "petnessadmin@gmail.com"
+  #define USER_PASSWORD "petness"
+
+  // Define Firebase Data Object
+  FirebaseData fbdo;
+  FirebaseAuth auth;
+  FirebaseConfig config;
+
+const int HX711_dout = 13; //mcu > HX711 dout pin
+const int HX711_sck = 27; //mcu > HX711 sck pin
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+unsigned long t = 0;
+
+void tokenStatusCallback(bool status) {
+  Serial.printf("Token status changed: %s\n", status ? "true" : "false");
+}
+
+void setup() {
+  Serial.begin(115200);
+
+      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+      Serial.print("Connecting to WiFi");
+      while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(300);
+      }
+      Serial.println("Connected to WiFi");
+
+      Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+      // Assign the api key (required)
+      config.api_key = API_KEY;
+
+      // Assign the user sign in credentials
+      auth.user.email = USER_EMAIL;
+      auth.user.password = USER_PASSWORD;
+
+      // Assign the callback function for the long running token generation task
+      config.token_status_callback = tokenStatusCallback;
+
+      Firebase.begin(&config, &auth);
+      Firebase.reconnectWiFi(true);
+}
+
+bool codeExecuted = false; // Flag to indicate whether the code has been executed
+
+void loop() {
+  
+    checkAndUpdateStatus();
+
+}
+
+void checkAndUpdateStatus() {
+    String path = "trigger";
+
+    if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", path.c_str(), "")) {
+        JsonDocument doc;
+        DeserializationError error = deserializeJson(doc, fbdo.payload().c_str());
+
+        if (!error) {
+            bool statusChanged = false; // Flag to indicate if status changed to true
+
+            for (JsonObject document : doc["documents"].as<JsonArray>()) {
+                const char *document_name = document["name"];
+                const bool statusValue = document["fields"]["status"]["booleanValue"];
+
+                if (strstr(document_name, "getPetWeight") != nullptr) {
+                    if (statusValue && !codeExecuted) {
+                        Serial.println("Status is ON");
+                        getPetsWeight();
+                        codeExecuted = true; // Set the flag to indicate that the code has been executed
+                        statusChanged = true; // Status changed to true
+                    } else if (!statusValue && codeExecuted) {
+                        // If status changed to false and codeExecuted is true, reset codeExecuted
+                        codeExecuted = false;
+                    }
+                }
+            }
+
+            if (!statusChanged && codeExecuted) {
+                // If status didn't change to true but code was executed, reset codeExecuted flag
+                codeExecuted = false;
+            }
+
+        } else {
+            Serial.print("deserializeJson() failed: ");
+            Serial.println(error.c_str());
+        }
+    }
+}
+
+void getPetsWeight () {
+
+// Load Cell Calibration Value
+  Serial.println("Load Cell Starting...");
+  float calibrationValue = 22.17; 
+
+// Load Cell TARE Precision
+  LoadCell.begin();
+  unsigned long stabilizingtime = 2000; 
+  boolean _tare = true; 
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Load Cell Startup Timeout, check MCU>HX711 wiring and pin designations");
+  } else {
+    LoadCell.setCalFactor(calibrationValue); Serial.println("Load Cell Startup is complete");
+  } // set calibration factor (float) 
+  
+  while (!LoadCell.update());
+    Serial.print("Calibration value: "); Serial.println(LoadCell.getCalFactor());
+    if (LoadCell.getSPS() < 7) {      
+      Serial.println("!!Sampling rate is lower than specification, check MCU>HX711 wiring and pin designations");
+    } else if (LoadCell.getSPS() > 100) {  
+      Serial.println("!!Sampling rate is higher than specification, check MCU>HX711 wiring and pin designations");
+    }
+
+
+  FirebaseJson content;
+  const int numSamples = 10; // Total number of samples to collect
+  const int numIgnoreSamples = 5; // Number of initial samples to ignore
+  float totalWeight = 0; // Variable to store the total weight
+  int sampleCount = 0; // Counter for the number of samples collected
+
+        // Ignore the first 5 samples
+      for (int i = 0; i < numIgnoreSamples; i++) {
+        bool newDataReady = false; // Flag to indicate new data
+        
+        while (!newDataReady) {
+          if (LoadCell.update()) newDataReady = true;
+        }
+        
+        if (newDataReady) {
+          float weightSample = LoadCell.getData(); // Get the weight sample
+          Serial.print("Ignoring sample: "); 
+          Serial.println(weightSample);
+        }
+        
+        delay(500); 
+      }
+      
+      // Collect the next 5 samples
+      for (int i = 0; i < numSamples - numIgnoreSamples; i++) {
+        bool newDataReady = false; // Flag to indicate new data
+        unsigned long t = millis(); // Store the current time
+        
+        while (!newDataReady) {
+          if (LoadCell.update()) newDataReady = true;
+        }
+        
+        if (newDataReady) {
+          float weightSample = LoadCell.getData(); // Get the weight sample
+          totalWeight += weightSample; // Add the weight to the total
+          sampleCount++; // Increment the sample count
+          Serial.print("Load_cell output val: "); 
+          Serial.println(weightSample);
+        }
+        
+        delay(500); // Delay for 1 second between samples
+      }
+      
+      // Calculate the mean average
+      float petsWeight = totalWeight / sampleCount;
+
+      // Convert weight to Kilograms
+      float petsWeightKg = petsWeight / 1000.0;
+      
+      // Print the mean average
+      Serial.print("PETS WEIGHT:  ");
+      Serial.println(petsWeightKg);
+
+                    // getting Weight Data and Sending to Firestore
+              String documentPath = "getWeight/LoadCell";
+              content.set("fields/Weight/stringValue", String(petsWeightKg, 2));
+              Serial.print("Updating Weight Data...");
+              if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw(), "Weight")) { 
+                Serial.println("ok");   
+              }
+              else  { 
+                Serial.print("failed, reason: "); Serial.println(fbdo.errorReason());
+              }
+
+              // UPDATING STATUS TO FALSE THAT ACT AS A TOGGLE OFF
+              String getPetWeightTrigger = "trigger/getPetWeight";
+              content.set("fields/status/booleanValue", boolean(false));
+              Serial.print("Updating getPetWeight Trigger Status Data...");
+              if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", getPetWeightTrigger.c_str(), content.raw(), "status")) { 
+                Serial.println("ok");   
+              } else  { 
+                Serial.print("failed, reason: "); Serial.println(fbdo.errorReason());
+              }
+              
+        totalWeight = 0;
+        sampleCount = 0;
+}
